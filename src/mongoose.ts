560a38a6133c89aae6ceb380a1d8c639be51a783
Vaibhav <vaibhavmonu8309@gmail.com>
2024-01-23 15:58:15 +0530

    refactor: get mongo db url from env

--- src/mongoose.ts
+++ src/mongoose.ts
@@ -1,11 +1,11 @@
 import mongoose from 'mongoose';
 import { Application } from './declarations';
 import logger from './logger';
+import dotenv from 'dotenv';
+dotenv.config();
 
 export default function (app: Application): void {
-  mongoose.connect(
-    app.get('mongodb')
-  ).catch(err => {
+  mongoose.connect(process.env.MONGODB_URL as string).catch(err => {
     logger.error(err);
     process.exit(1);
   });
