80903c636d053b3c0a372b3ef211deef8bf57141
Harsh Raj <harsh.smartters@gmail.com>
2024-01-26 23:07:19 +0530

    added comments

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,10 +2,12 @@ import { Application } from '../declarations';
 import users from './users/users.service';
 import reviews from './reviews/reviews.service';
 import faculties from './faculties/faculties.service';
+import comments from './comments/comments.service';
 // Don't remove this comment. It's needed to format import lines nicely.
 
 export default function (app: Application): void {
   app.configure(users);
   app.configure(reviews);
   app.configure(faculties);
+  app.configure(comments);
 }
